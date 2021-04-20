fcaeea0276c0bd9a8a5c267d74bc786f84646adf
MilthonB <milthonbor@gmail.com>
2021-04-20 00:33:22 -0700

    reset reactive carga inicial

--- formularios/src/app/pages/reactive/reactive.component.ts
+++ formularios/src/app/pages/reactive/reactive.component.ts
@@ -13,6 +13,7 @@ export class ReactiveComponent implements OnInit {
   constructor( private fb: FormBuilder) { 
 
     this.crearFormulario();
+    this.cargarDatosAlFormulario();
 
   }
 
@@ -33,6 +34,14 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   } 
 
+  get distritoNoValido(){
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  } 
+
+  get ciudadNoValido(){
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  } 
+
   crearFormulario(){
     //nombre:['valor que deseas establecer como defecto',
     //'validadores sincronos',
@@ -45,12 +54,29 @@ export class ReactiveComponent implements OnInit {
 
     //nombre: ['',],
       this.forma = this.fb.group({
-        nombre  : ['', [Validators.required, Validators.minLength(5)]],
-        apellido: ['', [Validators.required, Validators.minLength(5)]],
-        correo  : ['', [Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"), Validators.required]
-      ],
+        nombre   : ['', [Validators.required, Validators.minLength(5)]],
+        apellido : ['', [Validators.required, Validators.minLength(5)]],
+        correo   : ['', [Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"), Validators.required]],
+        direccion: this.fb.group({
+          distrito: ['', Validators.required],
+          ciudad  : ['',Validators.required],
+        })
       });
       
+  }
+
+
+  cargarDatosAlFormulario(){
+
+    this.forma.reset({
+      nombre: 'Lorenzo',
+      apellido: 'Primitivo',
+      correo: 'el_lore_69@xolo.com',
+      direccion: {
+        distrito: 'El Bandal',
+        ciudad: 'Los flacos'
+      }
+    });
   }
 
 
