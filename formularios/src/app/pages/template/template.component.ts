6bdfa65e249ed295ff199ffbb09c9dcec93e6be6
MilthonB <milthonbor@gmail.com>
2021-04-19 22:09:09 -0700

    api paises peticion get

--- formularios/src/app/pages/template/template.component.ts
+++ formularios/src/app/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from 'src/app/services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -15,9 +16,15 @@ export class TemplateComponent implements OnInit {
 
   }
 
-  constructor() { }
+  constructor( private paisSerive: PaisService ) { }
 
   ngOnInit(): void {
+
+      this.paisSerive.getPaises()
+          .subscribe( paises => {
+            console.log(paises);
+          });
+
   }
 
 
