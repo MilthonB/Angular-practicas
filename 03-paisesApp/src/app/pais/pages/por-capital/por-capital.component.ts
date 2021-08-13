0069f85cce6890c92017f375601615244f11618c
MB <milthonbor@gmail.com>
2021-08-13 16:26:22 -0700

    país y capital

--- 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-capital',
@@ -8,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
+  termino: string = "";
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService ) { }
 
   ngOnInit(): void {
   }
 
+  buscar( termino: string ){
+    this.hayError = false;
+  
+    console.log(this.termino)
+
+    this.termino = termino;
+    
+    this.paisService.buscarCapital( this.termino )
+        .subscribe( resp => {
+          console.log(resp);
+          this.paises = resp;
+        }, (err) => {
+          this.hayError = true;
+          this.paises = [];
+        });
+
+  }
+
+  sugerencias( termino: string ){
+    this.hayError = false;
+  }
+ 
 }
