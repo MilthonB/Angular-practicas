1cf78e2814ed7187cdf4ee7ac88bac6d6674fefb
MB <milthonbor@gmail.com>
2021-08-19 19:57:51 -0700

    rutas hijas

--- 05-heroesApp/src/app/auth/auth.module.ts
+++ 05-heroesApp/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
@@ -10,8 +11,9 @@ import { RegistroComponent } from './pages/registro/registro.component';
     LoginComponent,
     RegistroComponent
   ],
-  imports: [
-    CommonModule
+  imports: [ 
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
