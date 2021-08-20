11f3c0d329c7812cef56591ae64f1884f72bf6ee
MB <milthonbor@gmail.com>
2021-08-20 01:32:44 -0700

    rutas heroe, ver editar

--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -11,6 +11,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipePipe } from './pipes/imagen-pipe.pipe';
 
 
 
@@ -20,7 +22,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipePipe
   ],
   imports: [
     CommonModule,
