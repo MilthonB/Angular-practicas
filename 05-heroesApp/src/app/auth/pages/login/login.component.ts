18596ed0b6bba3a3f32f9988ef239e5e68f48c73
MB <milthonbor@gmail.com>
2021-08-21 21:29:36 -0700

    auth terminado

--- 05-heroesApp/src/app/auth/pages/login/login.component.ts
+++ 05-heroesApp/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Auth } from '../../interfaces/auth.interface';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  auth!: Auth;
+
+
+  constructor( private router: Router,
+               private authService: AuthService ) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    // ir al backend
+    // un usuario
+
+    this.authService.login().subscribe( resp => {
+      console.log(resp);
+      if( resp.id ){
+        this.router.navigate(['./heroes']);
+
+      }
+    })
+
+
   }
 
 }
