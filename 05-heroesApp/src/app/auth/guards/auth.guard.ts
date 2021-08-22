13c82d08abbe6260c56d7e996e9ecd66097d12bf
MB <milthonbor@gmail.com>
2021-08-22 03:08:22 -0700

    can active localstorage canload

--- 05-heroesApp/src/app/auth/guards/auth.guard.ts
+++ 05-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -1,25 +1,58 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+export class AuthGuard implements CanActivate, CanLoad {
+  
+  constructor( private authServices: AuthService,
+               private router : Router ){}
+
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
+    
+      // if( this.authServices.auth.id ){
+      //   return true;
+      // }
+
+      // console.log('Bloqueado por el AuthGuard - CanActivate');
+      // return false;
+      return this.authServices.verificaAutenticacion()
+                 .pipe(
+                   tap( estaAuteticado => {
+                     if(!estaAuteticado){
+                       this.router.navigate(['./auth/login']);  
+                     }
+                   } )
+                 );
+  }
 
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    console.log('canLoad', true);
-    console.log(route);
-    console.log(segments);
-      return true;
+   
+      return this.authServices.verificaAutenticacion()
+                 .pipe(
+                   tap( estaAuteticado => {
+                     if(!estaAuteticado){
+                       this.router.navigate(['./auth/login']);  
+                     }
+                   } )
+                 );
+      // if( this.authServices.auth.id ){
+      //   return true;
+      // }
+
+      // console.log('Bloqueado por el AuthGuard- CanLoad');
+      // return false;
   }
 
 }
