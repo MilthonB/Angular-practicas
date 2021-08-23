ed7b993e895ff054d19e9e9956348a5d3a11317b
MB <milthonbor@gmail.com>
2021-08-23 00:06:03 -0700

    diabled button

--- 06-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miformulatio') miformulario!: NgForm;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  nombrevalido(): boolean{
+    return this.miformulario?.controls.producto?.invalid &&
+           this.miformulario?.controls.producto?.touched
+  }
+
+  numeroValido(): boolean {
+    return this.miformulario?.controls.precio?.touched
+            && this.miformulario?.controls.precio?.value < 0
+  }
+
+  guardar( ){
+    console.log( this.miformulario.value )
   }
 
 }
