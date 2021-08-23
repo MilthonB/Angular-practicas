3f8a77a53afad9ac81f5435f66ed17e15c507235
MB <milthonbor@gmail.com>
2021-08-23 16:55:39 -0700

    elementos dinamicos

--- 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favoritos[]  
+}
+
+interface Favoritos {
+    id: number;
+    nombre: string;
+}
+
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,9 +19,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  nuevoJuego: string = "";
+
+  persona: Persona = {
+    nombre:'Milthon',
+    favoritos: [
+      {
+        id:1,
+        nombre:'Metal Gear'
+      },
+      {
+        id:2,
+        nombre:'Metal slugr'
+      }
+    ]
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  eliminar( index: number ){
+    this.persona.favoritos.splice(index,1)
+  }
+
+  guardar(){
+
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favoritos = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( {...nuevoFavorito} )  
+    this.nuevoJuego = ""
+  }
+
 
 }
