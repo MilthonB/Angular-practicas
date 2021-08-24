468b9ec434944119676e00820ddce8bfdef0ca45
MB <milthonbor@gmail.com>
2021-08-24 00:44:58 -0700

    validador personalizado

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // TODO: temporal
+  nombreApellidoPattern: string ="([a-zA-Z]+) ([a-zA-Z]+)";
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  noPuedeSerTonto( control: FormControl ){
+    const valor = control.value?.trim().toLowerCase();
+    if( valor === 'tonto' ){
+      return {
+        noTonto: true
+      }
+    }
+
+    return null
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern( this.nombreApellidoPattern )]],
+    email: ['', [Validators.required, Validators.pattern( this.emailPattern )]],
+    username: ['', [Validators.required, this.noPuedeSerTonto]],
+  })
+
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre:'Milthon B',
+      email:'tests1@gmail.com',
+    })
+  }
+
+  campoNoValido( campo: string ){
+    return this.miFormulario.get(campo)?.invalid 
+           && this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario(){
+
+    this.miFormulario.markAllAsTouched();
+
   }
 
 }
