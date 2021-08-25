fb59eb17cfe5dc24cc072a1fad605dbd23fe4cdd
MB <milthonbor@gmail.com>
2021-08-24 18:53:11 -0700

    cargar de modulos y rutas

---
+++ 07-selectoresApp/src/app/paises/pages/selector-paises/selector-paises.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-paises',
+  templateUrl: './selector-paises.component.html',
+  styles: [
+  ]
+})
+export class SelectorPaisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 07-selectoresApp/src/app/paises/paises.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PaisesRoutingModule } from './paises-routing.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    PaisesRoutingModule
+  ]
+})
+export class PaisesModule { }
