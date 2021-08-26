b9bdc0ac3b2d845dd8e8916b4d8becacd669bdca
MB <milthonbor@gmail.com>
2021-08-25 17:05:41 -0700

    implemetnacion de todods los ciclos de vida

--- 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
+++ 08-lifecycle/src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
@@ -6,11 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Pagina1Component implements OnInit {
+export class Pagina1Component implements 
+OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit'); 
+  }
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
 
   ngOnInit(): void {
+    console.log('ONInit');
+
   }
+
+ 
 
 }
