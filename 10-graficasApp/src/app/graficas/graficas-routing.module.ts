f49fb3fd4b3cc6423ea2faff9cb8768d005299ab
MB <milthonbor@gmail.com>
2021-08-26 20:12:14 -0700

    creacion de las rutas

--- 10-graficasApp/src/app/graficas/graficas-routing.module.ts
+++ 10-graficasApp/src/app/graficas/graficas-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { BarrasDonaComponent } from './pages/barras-dona/barras-dona.component';
+import { BarrasDonaHttpComponent } from './pages/barras-dona-http/barras-dona-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'barra',
+    component: BarrasComponent
+  },
+  {
+    path: 'barras-dobles',
+    component: BarrasDobleComponent
+  },
+  {
+    path: 'dona',
+    component: BarrasDonaComponent 
+  },
+  {
+    path: 'dona-http',
+    component: BarrasDonaHttpComponent 
+  },
+  {
+    path: '**',
+    redirectTo: 'barra' 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
