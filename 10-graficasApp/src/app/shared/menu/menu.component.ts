c4dfe8633c2880eba1733b578199c03121b87d89
MB <milthonbor@gmail.com>
2021-08-26 20:33:07 -0700

    no terminado

--- 10-graficasApp/src/app/shared/menu/menu.component.ts
+++ 10-graficasApp/src/app/shared/menu/menu.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem{
+    ruta: string;
+    texto: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    
+    ` 
+      li{
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class MenuComponent implements OnInit {
+
+  menu: MenuItem[] = [
+    { ruta: '/grafica/barra', texto: "Barras"},
+    { ruta: '/grafica/barras-dobles', texto: "Barras Dobles"},
+    { ruta: '/grafica/dona', texto: "Dona"},
+    { ruta: '/grafica/dona-http', texto: "Dona Http"},
+  ];
+
 
   constructor() { }
 
