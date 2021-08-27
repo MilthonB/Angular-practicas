4f779caedc336c2804b1dca482b5e03899b4995d
MB <milthonbor@gmail.com>
2021-08-27 16:29:03 -0700

    cargar direcativas

--- 11-directivasApp/src/app/shared/directives/error-msg.directive.ts
+++ 11-directivasApp/src/app/shared/directives/error-msg.directive.ts
@@ -1,17 +1,36 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, OnInit, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit {
 
-  constructor() { 
-    console.log('Directive')
-  }
+  htmlElement: ElementRef<HTMLElement>;
+  @Input() color: string = 'red';
+  @Input() mensaje: string = 'No hay nada';
+  
 
+  constructor( private el: ElementRef<HTMLElement> ) { 
+    this.htmlElement = el;
+    
+  }
 
   ngOnInit(): void {
     console.log('Directive-oninit')
+    this.setColor()
+    this.setMensaje()
+  }
+
+  setColor():void {
+
+    this.htmlElement.nativeElement.style.color = this.color
+    this.htmlElement.nativeElement.classList.add("form-text");
+    
+
+  }
+
+  setMensaje(): void{
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 
 }
