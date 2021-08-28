bf0c8e0e29d45d94320e6632b1306d38daa86ebf
MB <milthonbor@gmail.com>
2021-08-27 18:45:10 -0700

    carga de express

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,3 +1,19 @@
+const express = require('express');
 
 
-console.log('Hola')
+const app = express();
+
+app.get( '/', ( req, res ) => {
+    res.json(
+        {
+            ok: true,
+            msj: 'Todo salio bien',
+            uid: 123
+        }
+    )
+});
+
+
+app.listen( 4000, () => {
+    console.log(`El servidor esta corriendo en ${ 4000 }`)
+});
