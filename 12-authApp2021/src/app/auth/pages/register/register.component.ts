399a2a919f5709687b7aa1ee733365bfdb2e2544
MB <milthonbor@gmail.com>
2021-08-29 16:47:14 -0700

    movimiento entre pantallas

--- 12-authApp2021/src/app/auth/pages/register/register.component.ts
+++ 12-authApp2021/src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,11 +16,15 @@ export class RegisterComponent implements OnInit {
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
 
-  constructor( private fbuild: FormBuilder ) { }
+  constructor( private fbuild: FormBuilder,
+              private router: Router ) { }
 
   ngOnInit(): void {
   }
 
-  registrar(){}
+  registrar(){
+    this.router.navigateByUrl('/dashboard')
+
+  }
 
 }
