a7f9d6ab66877078dc028fe5f173282020dba118
MB <milthonbor@gmail.com>
2021-08-29 18:57:40 -0700

    headers cargados

--- 12-authApp2021/src/app/auth/services/auth.service.ts
+++ 12-authApp2021/src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/auth.interfaces';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { of } from 'rxjs';
@@ -31,6 +31,7 @@ export class AuthService {
                .pipe(
                  tap( resp => {
                     if(resp.ok){
+                      localStorage.setItem( 'token', resp.token! );
                       this._usuario = {
                         uid: resp.uid!,
                         name: resp.name!
@@ -40,6 +41,16 @@ export class AuthService {
                  map( resp => resp.ok ),
                  catchError( err => of(err.error.msg) )
                );
+
+  }
+
+  validarToken(){
+
+    const url : string = ` ${ this._baseUrl }auth/renew `;
+    const headers = new HttpHeaders()
+    .set('x-token', localStorage.getItem('token') || '');
+
+    this.http.get( url,{ headers } );
 
   }
 }
